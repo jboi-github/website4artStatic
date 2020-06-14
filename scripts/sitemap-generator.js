425c858d9738bb49c4a5aac9eb7e885ac7b63718
jboi <jboi.atwork@icloud.com>
2020-06-14 14:29:02 +0200

    sitemap generation III

--- scripts/sitemap-generator.js
+++ scripts/sitemap-generator.js
@@ -1,10 +1,11 @@
 require("babel-register") ({ presets: ["es2015", "react"] })
    
-const router = require("./scripts/sitemap-routes.js").default
+const router = require("./sitemap-routes.js").default
 const Sitemap = require("react-router-sitemap").default
-const profile = require("./public/config/profile.json")
+const profile = require("../public/config/profile.json")
 
 function generateSitemap() {
+    console.log(profile.site)
     return (
     new Sitemap(router)
         .build(profile.site)
