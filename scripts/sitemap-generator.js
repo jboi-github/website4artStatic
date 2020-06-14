23f16c294f9b3c242d022e3fa45e50f8a70d6c7e
jboi <jboi.atwork@icloud.com>
2020-06-14 14:03:16 +0200

    sitemap generation I

---
+++ scripts/sitemap-generator.js
@@ -0,0 +1,15 @@
+require("babel-register") ({ presets: ["es2015", "react"] })
+   
+const router = require("./scripts/sitemap-routes").default
+const Sitemap = require("react-router-sitemap").default
+const profile = require("./public/config/profile.json")
+
+function generateSitemap() {
+    return (
+    new Sitemap(router)
+        .build(profile.site)
+        .save("./public/config/sitemap.xml")
+    )
+}
+
+generateSitemap()
